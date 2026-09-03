bf08938d815ef2a9e7dcca5cae2712a30e92be35
Jarin Tasnim Sinigdha <jarintasnimsnigdha@gmail.com>
2026-09-03 21:07:55 +0600

    question 2 solved

--- answer.js
+++ answer.js
@@ -4,3 +4,20 @@ function describeValue(value) {
   const truthiness = value ? "truthy" : "falsy";
   return `${type} | ${truthiness}`;
 }
+//Question No. 2
+function getDayType(day) {
+  const d = day.toLowerCase();
+  switch (d) {
+    case "friday":
+    case "saturday":
+      return "Weekend";
+    case "sunday":
+    case "monday":
+    case "tuesday":
+    case "wednesday":
+    case "thursday":
+      return "Working Day";
+    default:
+      return "Invalid Day";
+  }
+}
