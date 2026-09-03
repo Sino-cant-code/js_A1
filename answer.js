720c9bc789acae6ea7172ed9f9b6aff02824d812
Jarin Tasnim Sinigdha <jarintasnimsnigdha@gmail.com>
2026-09-03 21:13:09 +0600

    question 3 & 4  solved

--- answer.js
+++ answer.js
@@ -21,3 +21,27 @@ function getDayType(day) {
       return "Invalid Day";
   }
 }
+
+
+//Question No. 3
+function validateUsername(username) {
+  if (username.length < 4) return "Too Short";
+  if (username.includes(" ")) return "No Space Allowed";
+  if (username.toLowerCase().includes("admin")) return "Reserved Word";
+  return "Available";
+}
+
+
+//Question No. 4
+function getCngFare(distance, isNight = false, waitingMinutes = 0) {
+  let fare = 50;
+  if (distance > 2) {
+    fare = 50 + (distance - 2) * 15;
+  }
+  const waitingCharge = waitingMinutes * 2;
+  let total = fare + waitingCharge;
+  if (isNight) {
+    total = total + total * 0.2;
+  }
+  return total;
+}
